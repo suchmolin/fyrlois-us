9267fc998b72a4eca6e8627d77d4be62a695381e
josesuchmolin <suchmolin11@gmail.com>
2024-08-23 08:01:02 -0400

    test page

--- src/app/test/page.jsx
+++ src/app/test/page.jsx
@@ -14,8 +14,9 @@ export default function Test() {
     phone: "",
   }
   const [answer, setAnswer] = useState([])
-  const [position, setPosition] = useState(58)
+  const [position, setPosition] = useState(0)
   const [personal, setPersonal] = useState(initialData)
+  const [result, setResult] = useState(-1)
 
   return (
     <div className="w-full flex flex-col items-center py-10 min-h-screen">
@@ -26,25 +27,36 @@ export default function Test() {
         Recuerda responder el examen de acuerdo con tus conocimientos, para que
         puedas conocer los resultados reales de tu nivel de inglés.
       </p>
-      <div className="w-6/12 h-[400px] relative rounded-md border shadow-xl my-10 p-7 overflow-hidden">
-        <h4 className="text-lg text-[#000b7a] text center">
-          Answer the following questions.
-        </h4>
-        {personal.email === "" && (
-          <PersonalDataTest setPersonal={setPersonal} />
-        )}
-        {personal.email !== "" && (
-          <QuestionsTest
-            position={position}
-            setPosition={setPosition}
-            answer={answer}
-            setAnswer={setAnswer}
-          />
-        )}
-      </div>
-      <p>
-        Pregunta {position + 1} de {testdata.length}
-      </p>
+
+      {result >= 0 && (
+        <p className="text-gray-500">
+          Has obtenido una puntuación de: {result} de {testdata.length}
+        </p>
+      )}
+      {result < 0 && (
+        <div className="w-6/12 h-[400px] relative rounded-md border shadow-xl my-10 p-7 overflow-hidden">
+          <h4 className="text-lg text-[#000b7a] text center">
+            Answer the following questions.
+          </h4>
+          {personal.email === "" && (
+            <PersonalDataTest setPersonal={setPersonal} />
+          )}
+          {personal.email !== "" && (
+            <>
+              <QuestionsTest
+                position={position}
+                setPosition={setPosition}
+                answer={answer}
+                setAnswer={setAnswer}
+                setResult={setResult}
+              />
+              <p className="absolute w-full bottom-0  text-gray-500 text-center py-4 z-0">
+                Pregunta {position + 1} de {testdata.length}
+              </p>
+            </>
+          )}
+        </div>
+      )}
     </div>
   )
 }
